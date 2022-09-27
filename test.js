9736f995223307a70cc74ca83715b6e49536ec71
Kevin Sookhan <kevinsookhan@Celises-MacBook-Pro.local>
2022-09-26 20:38:12 -0400

    week1day1

---
+++ test.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+console.log("hello" . length)
+console.log("hello" [1]);
+console.log("hello" . indexOf ('h'));
+console.log("hello".indexOf('l'));
+console.log("race".concat("car"))
+console.log("race"+"car");
+console.log("hello"+" "+"world"); 
+console.log(4 + "hi");
+console.log(4*"hi");
+console.log(4*"10");
+console.log("hello".toUpperCase());
+console.log("HELLO".toLowerCase());
+console.log("hello".slice());
+console.log("hello".slice(1));
+console.log("hello".slice(1,3));
+
+let test
+console.log(test);
+if (0) {
+    console.log(false);
+} else { 
+    console.log (true);
+}
